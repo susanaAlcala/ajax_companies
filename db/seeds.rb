af139a745beda9dbfc85fa6265d60116aff58eb8
Susana Alcala <susi.alcala@gmail.com>
2019-07-15 00:04:11 -0400

    Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Claim.destroy_all
-#Company.destroy_all
-#User.destroy_all
-#
-#
-#20.times do
-#  Company.create(
-#    name: Faker::Company.name
-#  )
-#end
-#
-#30.times do
-#  User.create(
-#    # name: Faker::FunnyName.name,
-#    email: Faker::Internet.email,
-#    password: 123456
-#  )
-#end
-#
-#users = User.all
-#companies = Company.all
-#
-#250.times do
-#  Claim.create(
-#    content: Faker::Lorem.sentence,
-#    user: users.sample,
-#    company: companies.sample
-#  )
-#end
+Claim.destroy_all
+Company.destroy_all
+User.destroy_all
+AdminUser.destroy_all
 
-AdminUser.create!(email: 'admin@gmail.com', password: '123123', password_confirmation: '123123') if Rails.env.development?
+20.times do
+  Company.create(
+    name: Faker::Company.name
+  )
+end
+
+30.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: 123456
+  )
+end
+
+users = User.all
+companies = Company.all
+
+250.times do
+  Claim.create(
+    content: Faker::Lorem.sentence,
+    user: users.sample,
+    company: companies.sample
+  )
+end
+AdminUser.create!(email: 'admin@gmail.com', password: '123123', password_confirmation: '123123') 
